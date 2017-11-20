a3110cc7b3a0d78b95914aec220ec1d7bb7f2fa2
CirQ <cirq999@163.com>
2017-11-20 14:53:34 +0800

    add visualization

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,7 @@
 # -*- coding: utf8 -*-
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 # The data preprocessing's logic is:
@@ -46,7 +48,7 @@ def opener():
         db.close()
     return df
 
-def clean(df):
+def clean(df: pd.DataFrame):
     before_size = df.shape[0]
     df = df[
         ((df['height'] > min_thr['height']) & (df['height'] < max_thr['height'])) |
@@ -60,11 +62,53 @@ def clean(df):
     after_size = df.shape[0]
     return before_size, after_size, df
 
-def max_min(df):
-    print(df.describe())
-    pass
+def split_sex(df: pd.DataFrame):
+    return df[df['sex']=='男'], df[df['sex']=='女']
+
+def max_min(df, ret=False):
+    h, w, l, sr, j, lr, b, t = df['height'], df['weight'], df['lung'], df['shortrun'], df['jump'], df['longrun'], df['bend'], df['two']
+    height = h[(h > min_thr['height']) & (h < max_thr['height'])].describe(percentiles=[])
+    weight = w[(w > min_thr['weight']) & (w < max_thr['weight'])].describe(percentiles=[])
+    lung = l[(l > min_thr['lung']) & (l < max_thr['lung'])].describe(percentiles=[])
+    shortrun = sr[(sr > min_thr['shortrun']) & (sr < max_thr['shortrun'])].describe(percentiles=[])
+    jump = j[(j > min_thr['jump']) & (j < max_thr['jump'])].describe(percentiles=[])
+    longrun = lr[(lr > min_thr['longrun']) & (lr < max_thr['longrun'])].describe(percentiles=[])
+    bend = b[(b > min_thr['bend']) & (b < max_thr['bend'])].describe(percentiles=[])
+    two = t[(t > min_thr['two']) & (t < max_thr['two'])].describe(percentiles=[])
+    stat = pd.DataFrame([height, weight, lung, shortrun, jump, longrun, bend, two])
+    if ret:
+        return stat
+    else:
+        print(stat)
+
+def filter_column(df, col):
+    return df[(df[col] > min_thr[col]) & (df[col] < max_thr[col])][col]
+
+def male_female_distribution_compare(mdf, fdf, col):
+    mseries = filter_column(mdf, col)
+    fseries = filter_column(fdf, col)
+    label = 'The Distribution of %s for Male' % col.capitalize()
+    sns.set_style('darkgrid', {'axes.facecolor':'0.9'})
+    plt.subplots(figsize=(10, 8))
+    plt.subplot(2, 1, 1)
+    plt.xlim(min_thr[col], max_thr[col])
+    sns.distplot(mseries, axlabel=label)
+    plt.subplot(2, 1, 2)
+    plt.xlim(min_thr[col], max_thr[col])
+    sns.distplot(fseries, axlabel=label)
+    plt.show()
+
+def draw_distribution(df, sex, col):
+    series = filter_column(df, col)
+    label = 'The Distribution of %s for %s' % (series.name.capitalize(), sex.capitalize())
+    sns.set_style('darkgrid', {'axes.facecolor':'0.9'})
+    sns.distplot(series, axlabel=label)
+    plt.show()
+
 
 
 if __name__ == '__main__':
     dataframe = opener()
-    max_min(dataframe)
+    maleframe, femaleframe = split_sex(dataframe)
+    male_female_distribution_compare(maleframe, femaleframe, 'longrun')
+    # draw_distribution(maleframe, 'male', 'height')
